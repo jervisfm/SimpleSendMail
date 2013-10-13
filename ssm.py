ee6a6a4f4092a479c05ce6952286ecec73a00b42
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-10-13 05:21:24 +0000

    Add basic main file

---
+++ ssm.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+__author__ = 'Jervis Muindi'
+__date__ = '12th October 2013'
+
+import sys
+
+def main(args):
+    print args
+
+if __name__ == '__main__':
+    main(sys.argv)
+
