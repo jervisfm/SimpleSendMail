7282fe053158a7bb7c61b0d253c46c7d2cd468be
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-10-13 06:04:23 +0000

    Add Command line help messages

--- ssm.py
+++ ssm.py
@@ -1,13 +1,31 @@
 #!/usr/bin/python
 
 __author__ = 'Jervis Muindi'
-__date__ = '12th October 2013'
+__date__ = 'October 2013'
 
+import argparse
 import sys
 
-def main(args):
-    print args
+def process_args():
+    """Process and parse the commandline arguments specified. 
+    
+    Returns:
+       A dictionary with given arguments as keys. 
+    """
+    parser = argparse.ArgumentParser(description='Simpler Interface to send mail')
+    parser.add_argument('--source', type=basestring, required=True, help='Source of email')
+    parser.add_argument('--destination', type=basestring, required=True, help='Destination to email to')
+    parser.add_argument('--subject', type=basestring, help='Subject of the message.')
+    parser.add_argument('--message', type=basestring, help='Main Body of the message.')
+    parser.add_argument('--file', type=basestring, help='Read specified text file and use that for the body of the message.') 
+    args = vars(parser.parse_args())
+    return args
+
+
+def main():
+    process_args()
+
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
 
